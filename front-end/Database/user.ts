dd97a05a09cc711907639162a8f69271d7a0f33f
ppgb912-wq <ppgb912@gmail.com>
2026-06-07 18:13:33 -0300

    Add birthDate field to User interface and data

--- front-end/Database/user.ts
+++ front-end/Database/user.ts
@@ -2,21 +2,25 @@ export interface User {
   id: number;
   name: string;
   ageMonths: number;
+  birthDate: string;
 }
 
 export const user: User[] = [  {
     id: 1,
     name: "Ricardo Silva",
     ageMonths: 516,
+  birthDate: "15/02/1983"
   },
   {
     id: 2,
     name: "Pedro Exemplo",
     ageMonths: 336,
+    birthDate: "16/05/1998"
   },
   {
     id: 3,
     name: "Maria Anti-vacina",
     ageMonths: 1188,
+    birthDate: "24/01/1926
   },
 ];
